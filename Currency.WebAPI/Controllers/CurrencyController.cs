f5bff95edc425f8baaadcd2fa5aac309c4e084d0
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2025-05-15 14:15:59 +0400

    Currency with memoryCache

---
+++ Currency.WebAPI/Controllers/CurrencyController.cs
@@ -0,0 +1,23 @@
+﻿using Currency.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Currency.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyService _service;
+
+        public CurrencyController(ICurrencyService service)
+        {
+            _service = service;
+        }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetCurrency()
+        {
+            var currency = await _service.GetCurrency();
+            return Ok(currency);
+        }
+    }
+}
